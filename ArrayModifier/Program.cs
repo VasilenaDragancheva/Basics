64834c9552324a3aeaa7c60312bcb2a2557ce6b3
Vasilena Dragancheva <vasilena86@hotmail.com>
2016-04-29 20:14:48 +0300

    Latest Tasks added

---
+++ ArrayModifier/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace ArrayModifier
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+
+    public class Program
+    {
+        public static BigInteger[] numbers;
+
+        public static void Main(string[] args)
+        {
+            numbers = Console.ReadLine().Split(' ').Select(BigInteger.Parse).ToArray();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "end")
+                {
+                    break;
+                }
+
+                var commandParams = line.Split(' ').ToArray();
+                ExecuteCommand(commandParams);
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        private static void ExecuteCommand(string[] commandParams)
+        {
+            var name = commandParams[0];
+            switch (name)
+            {
+                case "swap":
+                    SwapNumbers(commandParams);
+                    break;
+                case "multiply":
+                    Multiply(commandParams);
+                    break;
+                case "decrease":
+                    Decrease();
+                    break;
+            }
+        }
+
+        private static void Decrease()
+        {
+            numbers = numbers.Select(x => x -= 1).ToArray();
+        }
+
+        private static void Multiply(string[] commandParams)
+        {
+            int index1 = int.Parse(commandParams[1]);
+            int index2 = int.Parse(commandParams[2]);
+            numbers[index1] *= numbers[index2];
+        }
+
+        private static void SwapNumbers(string[] commandParams)
+        {
+            int index1 = int.Parse(commandParams[1]);
+            int index2 = int.Parse(commandParams[2]);
+            var number = numbers[index1];
+            numbers[index1] = numbers[index2];
+            numbers[index2] = number;
+        }
+    }
+}
---
+++ MultiplyTargetedCells/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace MultiplyTargetedCells
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static int[,] matrix;
+
+        public static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            matrix = new int[dimensions[0], dimensions[1]];
+
+            for (int r = 0; r < dimensions[0]; r++)
+            {
+                var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int c = 0; c < dimensions[1]; c++)
+                {
+                    matrix[r, c] = numbers[c];
+                }
+            }
+
+            int[] target = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int row = target[0];
+            int col = target[1];
+
+            int startRow = Math.Max(row - 1, 0);
+            int startCol = Math.Max(col - 1, 0);
+            int endRow = Math.Min(row + 1, dimensions[0] - 1);
+            int endCol = Math.Max(col + 1, dimensions[1] - 1);
+
+            int sum = 0;
+
+            for (int r = startRow; r <= endRow; r++)
+            {
+                for (int c = startCol; c <= endCol; c++)
+                {
+                    if (r == row && c == col)
+                    {
+                        continue;
+                    }
+
+                    sum += matrix[r, c];
+                }
+            }
+
+            for (int r = startRow; r <= endRow; r++)
+            {
+                for (int c = startCol; c <= endCol; c++)
+                {
+                    matrix[row, col] *= sum;
+                }
+            }
+
+            Render();
+        }
+
+        private static void Render()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                    if (j != matrix.GetLength(1) - 1)
+                    {
+                        Console.Write(matrix[i, j] + " ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
